130c73319588ea975283b70d840ba655a2a41f96
Rick Hallett <rickhallett@live.co.uk>
2021-06-09 11:15:45 +0100

    basic html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="progress-container">
+        <div className="progress-bar"></div>
+        <div className="circle active">1</div>
+        <div className="circle">2</div>
+        <div className="circle">3</div>
+        <div className="circle">4</div>
+      </div>
+
+      <div className="btn" id="prev">
+        Prev
+      </div>
+      <div className="btn" id="next">
+        Next
+      </div>
     </div>
   );
 }
